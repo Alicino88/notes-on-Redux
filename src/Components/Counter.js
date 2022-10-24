6a8e8a49808e5482f6b10bbe5b37791ca3312c00
Alice Moretti <ali.moret88@gmail.com>
2022-10-24 11:27:42 +0300

    Added increase/decrease logic inside counter component

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,16 +1,44 @@
-import { useSelector } from "react-redux";
-
-import classes from "./Counter.module.css";
-
+import { useSelector, useDispatch } from "react-redux";
+import { useRef } from "react";
 const Counter = () => {
+  const addedAmountRef = useRef();
+
+  const dispatch = useDispatch();
+  //by using useSelector the component is automatically subscribed to the store and gets the latest state snapshot
   const counter = useSelector((state) => state.counter);
 
+  const increaseAmount = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decreaseAmount = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const addToCounter = (e) => {
+    e.preventDefault();
+
+    const addedAmount = addedAmountRef.current.value;
+    const addedAmountNumber = +addedAmount;
+
+    dispatch({ type: "addMultiple", amount: addedAmountNumber });
+  };
   const toggleCounterHandler = () => {};
 
   return (
-    <main className={classes.counter}>
+    <main>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      <div>{counter}</div>
+      <div>
+        <button onClick={increaseAmount}>increment by one</button>
+
+        <button onClick={decreaseAmount}>decrement by one</button>
+      </div>
+      <form>
+        <label>Amount:</label>
+        <input type="number" ref={addedAmountRef}></input>
+        <button onClick={addToCounter}>Add</button>
+      </form>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,10 +1,15 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
   if (action.type === "increment") {
     return { counter: state.counter + 1 };
-  } else if (action.type === "decrement ") {
+  }
+  if (action.type === "decrement") {
     return { counter: state.counter - 1 };
+  }
+  if (action.type === "addMultiple") {
+    //action payload: extra property I can add to action object
+    return { counter: state.counter + action.amount };
   }
 
   return state;
