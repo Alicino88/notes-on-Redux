e5789f0dba47681d9bc87db322485845f4f6907e
Alice Moretti <ali.moret88@gmail.com>
2022-10-24 11:44:17 +0300

    added state for showCounter and action type

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,15 +1,27 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    return { counter: state.counter + 1, showCounter: state.showCounter };
   }
   if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+    return { counter: state.counter - 1, showCounter: state.showCounter };
   }
   if (action.type === "addMultiple") {
     //action payload: extra property I can add to action object
-    return { counter: state.counter + action.amount };
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
   }
 
   return state;
