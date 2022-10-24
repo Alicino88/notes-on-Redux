28dd617b9ed9152be93bb2b6be6f35fce2d65760
Alice Moretti <ali.moret88@gmail.com>
2022-10-24 13:29:28 +0300

    added comments to redux store

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,8 +2,14 @@ import { createStore } from "redux";
 
 const initialState = { counter: 0, showCounter: true };
 
+//the new state object overrides the previous one, therefore when updating a piece of state we also need to set the other states as well
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
+    //with redux we never change the existing state but we always override it by returning a brand new state object
+    //inside this block we cannot write:
+    //state.counter++
+    //return state
     return { counter: state.counter + 1, showCounter: state.showCounter };
   }
   if (action.type === "decrement") {
