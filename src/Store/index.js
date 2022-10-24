e125879204989469e0425b2e2b7a2538c4744b05
Alice Moretti <ali.moret88@gmail.com>
2022-10-24 15:06:22 +0300

    Creayed new authSlice and exported actions

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,12 +1,12 @@
 import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 //redux toolkit uses a package called Immer that prevents overriding the existing state
 const counterSlice = createSlice({
   name: "counter",
-  initialState: initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -21,6 +21,22 @@ const counterSlice = createSlice({
       state.showCounter = !state.showCounter;
     },
   },
+});
+
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+        state.isAuthenticated = true
+    },
+    logout(state) {
+        state.isAuthenticated = false
+    }
+
+
 });
 
 /*BELOW REDUX LOGIC WITHOUT REDUX-TOOLKIT
@@ -58,9 +74,12 @@ const counterReducer = (state = initialState, action) => {
 };
 */
 
+
+//even with multiple slices, configureStore is called only once:
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {counter: counterSlice.reducer, auth:authSlice.reducer}
 });
 //redux toolkit automatically creates actions from reducers names
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions
 export default store;
